4ca1065b06363734ca2ff8a77e8fc9c96b5ca4f3
Edina Kurto <122160209+EdinaKurto@users.noreply.github.com>
2024-04-23 02:31:12 +0200

    Update

---
+++ assets/js/comission_modal.js
@@ -0,0 +1,21 @@
+var modal = document.getElementById("edit_comission_modal");
+var span = document.getElementsByClassName("close")[0];
+
+
+var closeModalBtn = document.getElementById("closeModalBtn");
+
+editBtn.addEventListener('click', function () {
+
+ // Display the modal
+if (modal) {
+    modal.style.display = "block";
+}
+
+    return false;
+});
+
+span.onclick = function() {
+    if (modal) {
+        modal.style.display = "none";
+    }
+}
